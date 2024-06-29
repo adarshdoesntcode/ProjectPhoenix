b3e514e7642a68c16ff77cbdc97cf732e99aaf91
Adarsh Das <adarsh.191605@ncit.edu.np>
2024-06-29 22:12:59 +0545

    supervisor not content fix

--- src/features/supervisor/dashboard/SupervisorDashboard.jsx
+++ src/features/supervisor/dashboard/SupervisorDashboard.jsx
@@ -33,56 +33,72 @@ function SupervisorDashboard() {
   if (isLoading) {
     content = <Loader />;
   } else if (isSuccess) {
-    content = (
-      <main className="grid  items-start gap-4  md:gap-6 lg:grid-cols-2 xl:grid-cols-3">
-        <div className="grid auto-rows-max items-start gap-4 md:gap-6 lg:col-span-2">
-          <Card>
-            <CardHeader className="flex flex-row bg-slate-100 rounded-t-md border-b py-4 justify-between items-center">
-              <div>
-                <CardTitle className="text-lg">
-                  {`${getGreeting()}, ${user.fullname}`}
-                </CardTitle>
+    if (!projects) {
+      content = (
+        <div className="flex flex-1 items-center justify-center bg-slate-50 ">
+          <div className="flex flex-col items-center gap-1 text-center">
+            <h3 className="text-2xl font-bold tracking-tight">
+              No Projects Assigned !
+            </h3>
 
-                <CardDescription className="text-xs">
-                  These are the projects assigned to you
-                </CardDescription>
-              </div>
+            <p className="text-sm text-gray-500">
+              Projects will apprear when you are assigned any new projects
+            </p>
+          </div>
+        </div>
+      );
+    } else {
+      content = (
+        <main className="grid  items-start gap-4  md:gap-6 lg:grid-cols-2 xl:grid-cols-3">
+          <div className="grid auto-rows-max items-start gap-4 md:gap-6 lg:col-span-2">
+            <Card>
+              <CardHeader className="flex flex-row bg-slate-100 rounded-t-md border-b py-4 justify-between items-center">
+                <div>
+                  <CardTitle className="text-lg">
+                    {`${getGreeting()}, ${user.fullname}`}
+                  </CardTitle>
 
-              <Cctv className="text-slate-500" />
-            </CardHeader>
-
-            <CardContent className="pt-6">
-              {projects.data.length > 0 && (
-                <DataTable data={projects.data} columns={ProjectColumn} />
-              )}
-              {projects.data.length < 1 && (
-                <div className="h-[250px] font-semibold text-slate-800 flex justify-center items-center">
-                  No Projects are assigned to you currently!
+                  <CardDescription className="text-xs">
+                    These are the projects assigned to you
+                  </CardDescription>
                 </div>
-              )}
-            </CardContent>
-          </Card>
-        </div>
-        <div className="grid auto-rows-max items-start gap-4 md:gap-6 lg:col-span-2 xl:col-span-1">
-          <Card>
-            <CardHeader className="flex flex-row bg-slate-100 rounded-t-md border-b py-4 justify-between items-center">
-              <div>
-                <CardTitle className="text-lg">Events Timeline</CardTitle>
 
-                <CardDescription className="text-xs">
-                  Timeline of the ongoing events
-                </CardDescription>
-              </div>
-              <CalendarClock className="text-slate-500" />
-            </CardHeader>
+                <Cctv className="text-slate-500" />
+              </CardHeader>
 
-            <CardContent className="text-sm pt-8 pb-0 ">
-              <SupervisorEventsTimeline />
-            </CardContent>
-          </Card>
-        </div>
-      </main>
-    );
+              <CardContent className="pt-6">
+                {projects.data.length > 0 && (
+                  <DataTable data={projects.data} columns={ProjectColumn} />
+                )}
+                {projects.data.length < 1 && (
+                  <div className="h-[250px] font-semibold text-slate-800 flex justify-center items-center">
+                    No Projects are assigned to you currently!
+                  </div>
+                )}
+              </CardContent>
+            </Card>
+          </div>
+          <div className="grid auto-rows-max items-start gap-4 md:gap-6 lg:col-span-2 xl:col-span-1">
+            <Card>
+              <CardHeader className="flex flex-row bg-slate-100 rounded-t-md border-b py-4 justify-between items-center">
+                <div>
+                  <CardTitle className="text-lg">Events Timeline</CardTitle>
+
+                  <CardDescription className="text-xs">
+                    Timeline of the ongoing events
+                  </CardDescription>
+                </div>
+                <CalendarClock className="text-slate-500" />
+              </CardHeader>
+
+              <CardContent className="text-sm pt-8 pb-0 ">
+                <SupervisorEventsTimeline />
+              </CardContent>
+            </Card>
+          </div>
+        </main>
+      );
+    }
   } else if (isError) {
     content = <ApiError error={error} />;
   }
