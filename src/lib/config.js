3b04091856dde8a6b6f994b465896f6f71db6989
Adarsh Das <adarsh.191605@ncit.edu.np>
2024-07-03 20:56:37 +0545

    push

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,6 +1,8 @@
 export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
   "https://project-phoenix-clz.vercel.app/api";
+
+console.log(import.meta.env.VITE_API_BASE_URL);
 // export const API_BASE_URL = "https://project-phoenix-omega.vercel.app/api";
 // export const API_BASE_URL = "http://localhost:3500/api";
 
